9a59d08f2faa0cd2130e659cb633c0614e0ec88a
baodongdong <baodongdong@tigerjoys.com>
2018-02-27 18:46:04 +0800

    添加注册,验证等接口

--- src/main/java/com/mmall/common/ResponseCode.java
+++ src/main/java/com/mmall/common/ResponseCode.java
@@ -10,7 +10,6 @@ public enum ResponseCode {
     private final int code;
     private final String desc;
 
-
     ResponseCode(int code,String desc){
         this.code = code;
         this.desc = desc;
--- src/main/java/com/mmall/dao/UserMapper.java
+++ src/main/java/com/mmall/dao/UserMapper.java
@@ -19,4 +19,10 @@ public interface UserMapper {
     int checkUsername(String username);
 
     User selectLogin(@Param("username")String username, @Param("password")String password);
+
+    int checkEmail(String email);
+
+    String selectQuestionByUsername(String username);
+
+    int checkAnswer(@Param("username")String username, @Param("question")String question, @Param("answer")String answer);
 }
