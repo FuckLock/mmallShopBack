c1a1835cc21a901536ae98910463b00c2e60f8bb
baodongdong <baodongdong@tigerjoys.com>
2018-03-05 16:02:14 +0800

    完成分类接口开发并通过接口测试

---
+++ src/main/java/com/mmall/service/ICategoryService.java
@@ -0,0 +1,13 @@
+package com.mmall.service;
+
+import com.mmall.common.ServerResponse;
+
+public interface ICategoryService {
+    ServerResponse addCategory(String categoryName, Integer parentId);
+
+    ServerResponse updateCategoryName(Integer categoryId, String categoryName);
+
+    ServerResponse getChildrenParallelCategory(Integer categoryId);
+
+    ServerResponse selectCategoryAndChildrenById(Integer categoryId);
+}
