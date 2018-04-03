6b4c7c2416fbc74a2a50da6be9476ded1d796c40
baodongdong <baodongdong@tigerjoys.com>
2018-04-03 14:40:32 +0800

    添加springmvc 全局异常处理并修改logback.xml文件

---
+++ src/main/java/com/mmall/common/ExceptionResolver.java
@@ -0,0 +1,28 @@
+package com.mmall.common;
+
+import com.mmall.util.PropertiesUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.json.MappingJacksonJsonView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class ExceptionResolver implements HandlerExceptionResolver {
+    private static Logger logger = LoggerFactory.getLogger(ExceptionResolver.class);
+
+    @Override
+    public ModelAndView resolveException(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) {
+        logger.error("访问请求{} Exception", httpServletRequest.getRequestURI(), e);
+        ModelAndView modelAndView = new ModelAndView(new MappingJacksonJsonView());
+
+        modelAndView.addObject("status",ResponseCode.ERROR.getCode());
+        modelAndView.addObject("msg","接口异常,详情请查看服务端日志的异常信息");
+        modelAndView.addObject("data", e.toString());
+        return modelAndView;
+    }
+}
