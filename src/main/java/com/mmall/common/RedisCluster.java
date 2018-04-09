a2029b23804cab0370f625a2a625b98efb6b6680
baodongdong <baodongdong@tigerjoys.com>
2018-04-09 16:48:49 +0800

    实现使用redis-cluster来集群取代redis sharding方式来集群

--- src/main/java/com/mmall/common/RedisCluster.java
+++ src/main/java/com/mmall/common/RedisCluster.java
@@ -1,19 +1,17 @@
 package com.mmall.common;
 
 import com.mmall.util.PropertiesUtil;
-import redis.clients.jedis.JedisPoolConfig;
-import redis.clients.jedis.JedisShardInfo;
-import redis.clients.jedis.ShardedJedis;
-import redis.clients.jedis.ShardedJedisPool;
+import redis.clients.jedis.*;
 import redis.clients.util.Hashing;
 import redis.clients.util.Sharded;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
-// 在redis 3.0出来之前是使用redis sharding来实现redis集群的下面为分片方式, 采用的redis版本是2.8.0
-public class RedisShardedPool {
-    private static ShardedJedisPool pool;//sharded jedis连接池
+// redis-3.0 采用如下方式写集群,使用redis cluster 在redis服务器端要建立集群
+public class RedisCluster {
+
+    private static JedisCluster jedisCluster;
     private static Integer maxTotal = Integer.parseInt(PropertiesUtil.getProperty("redis.max.total","20")); //最大连接数
     private static Integer maxIdle = Integer.parseInt(PropertiesUtil.getProperty("redis.max.idle","20"));//在jedispool中最大的idle状态(空闲的)的jedis实例的个数
     private static Integer minIdle = Integer.parseInt(PropertiesUtil.getProperty("redis.min.idle","20"));//在jedispool中最小的idle状态(空闲的)的jedis实例的个数
@@ -29,59 +27,33 @@ public class RedisShardedPool {
 
     private static void initPool(){
         JedisPoolConfig config = new JedisPoolConfig();
-
         config.setMaxTotal(maxTotal);
         config.setMaxIdle(maxIdle);
         config.setMinIdle(minIdle);
-
         config.setTestOnBorrow(testOnBorrow);
         config.setTestOnReturn(testOnReturn);
+        config.setBlockWhenExhausted(true);
+        Set<HostAndPort> clusterNodes = new HashSet<HostAndPort>();
+        clusterNodes.add(new HostAndPort(redis1Ip, redis1Port));
+        clusterNodes.add(new HostAndPort(redis2Ip, redis2Port));
+        jedisCluster = new JedisCluster(clusterNodes, 5000, config);
+    }
 
-        config.setBlockWhenExhausted(true);//连接耗尽的时候，是否阻塞，false会抛出异常，true阻塞直到超时。默认为true。
-
-        JedisShardInfo info1 = new JedisShardInfo(redis1Ip, redis1Port,1000*2);
-
-        JedisShardInfo info2 = new JedisShardInfo(redis2Ip, redis2Port,1000*2);
-
-        List<JedisShardInfo> jedisShardInfoList = new ArrayList<JedisShardInfo>(2);
-
-        jedisShardInfoList.add(info1);
-        jedisShardInfoList.add(info2);
-
-         pool = new ShardedJedisPool(config, jedisShardInfoList, Hashing.MURMUR_HASH, Sharded.DEFAULT_KEY_TAG_PATTERN);
+    public static JedisCluster getJedisCluster(){
+        return jedisCluster;
     }
 
     static{
         initPool();
     }
 
-    public static ShardedJedis getJedis(){
-        return pool.getResource();
-    }
-
-
-    public static void returnBrokenResource(ShardedJedis jedis){
-        pool.returnBrokenResource(jedis);
-    }
-
-
-
-    public static void returnResource(ShardedJedis jedis){
-        pool.returnResource(jedis);
-    }
-
-
-    public static void main(String[] args) {
-//        ShardedJedis jedis = pool.getResource();
+//    public  static void  main(String[] args){
+//        JedisCluster jedis = RedisCluster.getJedisCluster();
 //
 //        for(int i =0;i<10;i++){
 //            jedis.set("key"+i,"value"+i);
 //        }
-//        returnResource(jedis);
 //
-////        pool.destroy();//临时调用，销毁连接池中的所有连接
 //        System.out.println("program is end");
-
-
-    }
+//    }
 }
